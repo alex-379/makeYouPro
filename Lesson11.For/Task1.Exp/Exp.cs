43b1484e6773764056c1bf28367a7a4553eff9ce
alex-379 <alexey_379@mail.ru>
2022-11-13 20:33:56 +1000

    Add project files.

--- Lesson11.For/Task1.Exp/Exp.cs
+++ Lesson11.For/Task1.Exp/Exp.cs
@@ -2,5 +2,9 @@
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число B: ");
 int b = Convert.ToInt32(Console.ReadLine());
-double exp = Math.Pow(a, b);
-Console.WriteLine ("Число A в степени B: " + exp);
+int exp = 1;
+for (int i = 1; i <= b; i++)
+{
+    exp *= a;
+}
+Console.WriteLine("Число A в степени B: " + exp);
