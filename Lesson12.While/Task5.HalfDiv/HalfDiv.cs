254d0dcaa2c5efc244f874f80f09f761c809b34f
alex-379 <alexey_379@mail.ru>
2022-11-13 16:34:33 +1000

    Add project files.

--- Lesson12.While/Task5.HalfDiv/HalfDiv.cs
+++ Lesson12.While/Task5.HalfDiv/HalfDiv.cs
@@ -2,8 +2,8 @@
 int s = Convert.ToInt32(Console.ReadLine());
 double a = 1;
 double b = s;
-double exp = 0.00000001;
-while (Math.Abs(b - a) > exp)
+double eps = 0.00000001;
+while (Math.Abs(b - a) > eps)
 {
     double c = (a + b) / 2;
     if ((a * a * a - s) * (c * c * c - s) < 0)
