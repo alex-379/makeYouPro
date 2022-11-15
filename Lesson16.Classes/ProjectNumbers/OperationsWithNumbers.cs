0ee88dc8abdcd0d157a00894670bbf522e24689f
alex-379 <alexey_379@mail.ru>
2022-11-16 06:16:57 +1000

    Add project files.

---
+++ Lesson16.Classes/ProjectNumbers/OperationsWithNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectNumbers
+{
+    public static class OperationsWithNumbers
+    {
+
+        public static double ValueOfLinearEqution(double a, double b, double c)
+        {
+            double x = (c - b) / a;
+            return (x);
+        }
+
+        public static double SumIfAMoreMultiplyIfEqualDifferenceIfBMore(double a, double b)
+        {
+            double result = 0;
+            if (a > b) result = a + b;
+            if (a == b) result = a * b;
+            if (a < b) result = a - b;
+            return (result);
+        }
+       /* public static int NumberToString(int a)
+        {
+
+            return ();
+        }*/
+    }
+}
---
+++ Lesson16.Classes/ProjectNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ProjectNumbers;
+
+using ProjectNumbers;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число A: ");
+        string aString = Console.ReadLine();
+        int a = Convert.ToInt32(aString);
+
+        Console.WriteLine("Введите число B: ");
+        string bString = Console.ReadLine();
+        int b = Convert.ToInt32(bString);
+
+        Console.WriteLine("Введите число C: ");
+        string cString = Console.ReadLine();
+        int c = Convert.ToInt32(cString);
+
+        double res1 = OperationsWithNumbers.ValueOfLinearEqution(a, b, c);
+        Console.WriteLine($"\nРешение линейного уравнения равно: {res1}");
+
+        double res2 = OperationsWithNumbers.SumIfAMoreMultiplyIfEqualDifferenceIfBMore(a, b);
+        Console.WriteLine($"\nРезультат действия с числами равен: {res2}");
+    }
+}
