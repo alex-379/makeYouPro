f759139572f4fe4aa4194aa9a85b3a1971bf5770
alex-379 <alexey_379@mail.ru>
2022-12-24 22:26:54 +1000

    Add project files.

--- Lesson19.ArraysMultidimensional/ProjectArraysMultidimensional/OperationsWithArraysMultidimensional.cs
+++ Lesson19.ArraysMultidimensional/ProjectArraysMultidimensional/OperationsWithArraysMultidimensional.cs
@@ -1,10 +1,11 @@
 ﻿namespace ProjectArraysMultidimensional;
 public static class OperationsWithArraysMultidimensional
 {
-    public static int[,] Generate(int column, int row, int left, int right)
+    public static int[,] Generate(int row, int column, int left, int right)
     {
-        int[,] array = new int[column, row];
-        Random random= new();
+        int[,] array = new int[row, column];
+        Random random = new();
+
         for (int i = 0; i < array.GetLength(0); i++)
         {
             for (int j = 0; j < array.GetLength(1); j++)
@@ -16,9 +17,21 @@ public static class OperationsWithArraysMultidimensional
         return array;
     }
 
-    public static void Write(int[,] array, string nameResult)
+    public static void Write(int[] array, string nameArray)
     {
-        Console.Write($"{nameResult}:\n");
+        Console.Write($"{nameArray}:");
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.Write("{0}", array[i]);
+        }
+        Console.WriteLine();
+    }
+
+    public static void Write(int[,] array, string nameArray)
+    {
+        Console.Write($"{nameArray}:\n");
+
         for (int i = 0; i < array.GetLength(0); i++)
         {
             for (int j = 0; j < array.GetLength(1); j++)
@@ -32,5 +45,72 @@ public static class OperationsWithArraysMultidimensional
     public static void Write(int result, string nameResult)
     {
         Console.WriteLine($"{nameResult}: {result}\n");
+    }
+
+    public static int GetMinElementArray(int[,] array, out int[] minIndex)
+    {
+        Console.WriteLine();
+
+        int min = array[0, 0];
+        minIndex = new int [2];
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (min > array[i, j])
+                {
+                    min = array[i, j];
+                    minIndex[0] = j;
+                    minIndex[1] = i;
+                }
+            }
+        }
+
+        return min;
+    }
+
+    public static int GetMaxElementArray(int[,] array, out int[] maxIndex)
+    {
+        Console.WriteLine();
+
+        int max = array[0, 0];
+        maxIndex = new int[2];
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (max < array[i, j])
+                {
+                    max = array[i, j];
+                    maxIndex[0] = j;
+                    maxIndex[1] = i;
+                }
+            }
+        }
+
+        return max;
+    }
+
+    public static int GetCountElementsMoreLeftAndDown(int[,] array)
+    {
+        Console.WriteLine();
+
+        int count = 0;
+
+        for (int i = 0; i < array.GetLength(0)-1; i++)
+        {
+            for (int j = 1; j < array.GetLength(1); j++)
+            {
+                if (array[i, j] > array[i+1, j] &&
+                    array[i, j] > array[i, j-1])
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 }
--- Lesson19.ArraysMultidimensional/ProjectArraysMultidimensional/Program.cs
+++ Lesson19.ArraysMultidimensional/ProjectArraysMultidimensional/Program.cs
@@ -1,4 +1,15 @@
 ﻿using static ProjectArraysMultidimensional.OperationsWithArraysMultidimensional;
 
-int[,] matrix = Generate(3,3,100,201);
+int[,] matrix = Generate(5,3,100,201);
 Write(matrix, "Исходная матрица");
+
+int minimum = GetMinElementArray(matrix, out int[] minIndex);
+Write(minimum, "Минимальный элемент массива");
+Write(minIndex, "Индекс минимального элемента массива");
+
+int maximum = GetMaxElementArray(matrix, out int[] maxIndex);
+Write(maximum, "Максимальный элемент массива");
+Write(maxIndex, "Индекс максимального элемента массива");
+
+int count = GetCountElementsMoreLeftAndDown(matrix);
+Write(count, "Количество элементов массива, больше своих левого и нижнего соседа одновременно");
