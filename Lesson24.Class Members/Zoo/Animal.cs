29cfe04d677afff1901f3edf58b6371d8738b9f7
alex-379 <alexey_379@mail.ru>
2023-01-30 21:20:07 +1000

    Add project files.

--- Lesson24.Class Members/Zoo/Animal.cs
+++ Lesson24.Class Members/Zoo/Animal.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Text.RegularExpressions;
 
 namespace Zoo;
 
@@ -10,77 +10,159 @@ public class Animal
 
     public int Age { get; set; }
 
-    public Animal(string name, int volume, int age)
+    public string Type { get; set; }
+
+    public string Biom { get; set; }
+
+    public int Square { get; set; }
+
+    public string Food { get; set; }
+
+    public string Group { get; set; }
+
+    public string Sound { get; set; }
+
+    public Animal(string name, int volume, int age, string type, string biom, int square, string food, string group, string sound)
     {
         Name = name;
         Volume = volume;
         Age = age;
+        Type = type;
+        Biom = biom;
+        Square = square;
+        Food = food;
+        Group = group;
+        Sound = sound;
     }
 
-    private string _type = "";
-    private string _biom = "";
-    private int _square;
-    private string _group = "";
-    private string _food = "";
-    private string _sound = "";
-
-    public void DescribeElefanth()
+    public void DescribeAnimal()
     {
-        _type = "Слон";
-        _biom = "тропики";
-        _square = 10;
-        _group = "травоядное";
-        _food = "сено";
-        _sound = "Ауф";
-
-        Console.WriteLine($"{_type} {Name} обитает в биоме {_biom} на площади {_square} м^2.\n" +
-                          $"Его возраст в годах - {Age}. Это {_group}.\n" +
-                          $"Рацион питания - {_food} в количестве {Volume} кг в сутки.\n" +
-                          $"Издаваемый звук - \"{_sound}\".\n");
-    }
-
-    public void DescribePenguin()
-    {
-        _type = "Пингвин";
-        _biom = "тундра";
-        _square = 10;
-        _group = "хищник";
-        _food = "рыба";
-        _sound = "Хрю";
-
-        Console.WriteLine($"{_type} {Name} обитает в биоме {_biom} на площади {_square} м^2.\n" +
-                          $"Его возраст в годах - {Age}. Это {_group}.\n" +
-                          $"Рацион питания - {_food} в количестве {Volume} кг в сутки.\n" +
-                          $"Издаваемый звук - \"{_sound}\".\n");
-    }
-
-    public void DescribeTiger()
-    {
-        _type = "Тигр";
-        _biom = "пустыня";
-        _square = 10;
-        _group = "хищник";
-        _food = "рыба и мясо";
-        _sound = "Рррр";
-
-        Console.WriteLine($"{_type} {Name} обитает в биоме {_biom} на площади {_square} м^2.\n" +
-                          $"Его возраст в годах - {Age}. Это {_group}.\n" +
-                          $"Рацион питания - {_food} в количестве {Volume} кг в сутки.\n" +
-                          $"Издаваемый звук - \"{_sound}\".\n");
+        Console.WriteLine($"{Type} {Name} обитает в биоме {Biom} на площади {Square} м^2.\n" +
+                          $"Его возраст в годах - {Age}. Это {Group}.\n" +
+                          $"Рацион питания - {Food} в количестве {Volume} кг в сутки.\n" +
+                          $"Издаваемый звук - \"{Sound}\".\n");
     }
 
     public void Eat()
     {
-        Console.WriteLine($"{_type} {Name} ест.\n");
+        Console.WriteLine($"{Type} {Name} ест.\n");
     }
 
     public void MakeSound()
     {
-        Console.WriteLine($"{_type} {Name} издаёт звук {_sound}.\n");
+        Console.WriteLine($"{Type} {Name} издаёт звук {Sound}.\n");
     }
 
     public void Play()
     {
-        Console.WriteLine($"{_type} {Name} играет.\n");
+        Console.WriteLine($"{Type} {Name} играет.\n");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*public string Name { get; set; }
+
+public int Volume { get; set; }
+
+public int Age { get; set; }
+
+public Animal(string name, int volume, int age)
+{
+    Name = name;
+    Volume = volume;
+    Age = age;
+}
+
+private string _type = "";
+private string _biom = "";
+private int _square;
+private string _group = "";
+private string _food = "";
+private string _sound = "";
+
+public void DescribeElefanth()
+{
+    _type = "Слон";
+    _biom = "тропики";
+    _square = 10;
+    _group = "травоядное";
+    _food = "сено";
+    _sound = "Ауф";
+
+    Console.WriteLine($"{_type} {Name} обитает в биоме {_biom} на площади {_square} м^2.\n" +
+                      $"Его возраст в годах - {Age}. Это {_group}.\n" +
+                      $"Рацион питания - {_food} в количестве {Volume} кг в сутки.\n" +
+                      $"Издаваемый звук - \"{_sound}\".\n");
+}
+
+public void DescribePenguin()
+{
+    _type = "Пингвин";
+    _biom = "тундра";
+    _square = 10;
+    _group = "хищник";
+    _food = "рыба";
+    _sound = "Хрю";
+
+    Console.WriteLine($"{_type} {Name} обитает в биоме {_biom} на площади {_square} м^2.\n" +
+                      $"Его возраст в годах - {Age}. Это {_group}.\n" +
+                      $"Рацион питания - {_food} в количестве {Volume} кг в сутки.\n" +
+                      $"Издаваемый звук - \"{_sound}\".\n");
+}
+
+public void DescribeTiger()
+{
+    _type = "Тигр";
+    _biom = "пустыня";
+    _square = 10;
+    _group = "хищник";
+    _food = "рыба и мясо";
+    _sound = "Рррр";
+
+    Console.WriteLine($"{_type} {Name} обитает в биоме {_biom} на площади {_square} м^2.\n" +
+                      $"Его возраст в годах - {Age}. Это {_group}.\n" +
+                      $"Рацион питания - {_food} в количестве {Volume} кг в сутки.\n" +
+                      $"Издаваемый звук - \"{_sound}\".\n");
+}
+
+public void Eat()
+{
+    Console.WriteLine($"{_type} {Name} ест.\n");
+}
+
+public void MakeSound()
+{
+    Console.WriteLine($"{_type} {Name} издаёт звук {_sound}.\n");
+}
+
+public void Play()
+{
+    Console.WriteLine($"{_type} {Name} играет.\n");
+}
+}
+*/
--- Lesson24.Class Members/Zoo/Program.cs
+++ Lesson24.Class Members/Zoo/Program.cs
@@ -1,10 +1,49 @@
 ﻿using Zoo;
 
-string name = "Симба";
+Animal animal = new ("Melman", 30, 20, "жираф", "саввана", 10, "листья акации", "травоядное", "х-ы-ы");
+animal.DescribeAnimal();
+
+animal.Play();
+animal.Eat();
+animal.MakeSound();
+
+animal.Type = "кордофанский жираф";
+animal.Name = "Африка";
+animal.Biom = "тропический лес";
+animal.DescribeAnimal();
+animal.Eat();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*string name = "Симба";
 int volume = 50;
 int age = 20;
 Animal elefanth = new(name, volume, age);
 elefanth.DescribeElefanth();
+
 
 name = "Петя";
 volume = 5;
@@ -29,4 +68,4 @@ penguin.Play();
 
 penguin.Eat();
 elefanth.MakeSound();
-tiger.Play();
+tiger.Play();*/
