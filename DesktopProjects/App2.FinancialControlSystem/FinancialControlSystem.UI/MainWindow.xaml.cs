70c7349984e7e2dcec5db8ed9fede107c935c0f4
alex-379 <alexey_379@mail.ru>
2023-12-24 17:34:31 +1000

    Initializing Project

---
+++ DesktopProjects/App2.FinancialControlSystem/FinancialControlSystem.UI/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace FinancialControlSystem.UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
